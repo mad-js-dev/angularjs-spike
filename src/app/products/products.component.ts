f5b61db14b1c3d6c2022a45c3a8d3357f5ebd8d1
Brais <braisva@gmail.com>
2018-08-15 20:06:29 +0200

    rename component

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+
+export class ProductComponent implements OnInit {
+
+  selectedProduct: Product;
+  
+  products = Product[];
+  
+  constructor(private productService:ProductService) { }
+
+
+  ngOnInit() {
+    this.getProducts();
+  }
+  /*
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+  }
+  
+  getProducts(): void {
+    this.roductService.getProducts()
+        .subscribe(products => this.products = products);
+  }*/
+}
